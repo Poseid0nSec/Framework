227bff7bc989b2d3938b022d629837ce52ed5508
ReddyyZ <arthurc.oli.whitehat@gmail.com>
2021-02-15 11:43:11 -0300

    Added multiple targets and ping check

--- modules/portscanner.py
+++ modules/portscanner.py
@@ -1,8 +1,73 @@
 import logging
 import argparse
+import ipaddress
 from datetime import datetime
 from modules.module import Module
 from scapy.all import *
+from threading import Thread
+
+
+class ThreadWithReturnValue(Thread):
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                        **self._kwargs)
+
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
+
+
+def handle_targets(targets):
+    targets_ = []
+
+    try:
+        targets_.append(format(ipaddress.ip_address(targets)))
+    except ipaddress.AddressValueError or ValueError:
+        try:
+            for i in ipaddress.ip_network(targets):
+                targets_.append(format(i))
+
+            targets_.pop(0)
+        except ValueError:
+            logging.critical("ValueError: Use an valid IP Address!")
+
+            return False
+
+    return targets_
+
+
+def ping_target(target):
+    pkt = IP(dst=target) / ICMP()
+    ans, _ = sr(pkt, retry=2, timeout=1, verbose=0)
+
+    if not ans:
+        return False
+
+    return target
+
+
+def check_target_is_online(targets: list):
+    targets_ = []
+    threads = []
+
+    for target in targets:
+        t = ThreadWithReturnValue(target=ping_target, args=(target,))
+        t.start()
+        threads.append(t)
+
+    for thread in threads:
+        res = thread.join()
+
+        if res:
+            targets_.append(res)
+
+    return targets_
 
 
 class PortScanner(Module):
@@ -14,7 +79,70 @@ class PortScanner(Module):
         "TCP-Connect",
         "TCP-SYN",
     ]
+    required_options = [
+        "target",
+        "port",
+    ]
     timeout = 2
+
+    def initialize(self, options: dict):
+        self.arguments = options
+
+        if options[self.optname]:
+            for opt in self.required_options:
+                if not options[opt]:
+                    return logging.critical(f"Required argument: \"{opt}\"")
+
+            begin_time = datetime.now()
+            logging.debug(f"Starting port scan!")
+
+            # Handle arguments
+            targets = handle_targets(options["target"])
+
+            port = options["port"].split("-")
+            port = [int(i) for i in port]
+
+            scan_type = "TCP-SYN" if options["syn_scan"] else "TCP-Connect"
+            dont_ping = options["dont_ping"]
+
+            # Check arguments
+            if not targets or not port:
+                return False
+
+            # Init portscan
+
+            self.main(targets, port, scan_type, dont_ping)
+
+            logging.debug(f"Port scan finished in {datetime.now() - begin_time}")
+
+    def options(self, parser: argparse.ArgumentParser):
+        parser.add_argument("-sS", "--syn-scan", help="Execute SYN Scan (Stealth)", action="store_true")
+        parser.add_argument("-t", "--target", help="Target IP", type=str)
+        parser.add_argument("-p", "--port", help="Port range to scan", type=str)
+        parser.add_argument("-dP", "--dont-ping", help="Don't check if host is up", action="store_true")
+
+    def main(self, targets: list, port_range: [int, int], scan_type: str, dont_ping: bool):
+        if not dont_ping:
+            logging.debug("Checking targets are up")
+            targets = check_target_is_online(targets)
+
+            if not targets:
+                return logging.critical("Targets offline!")
+            logging.info(f"{len(targets)} {'targets' if len(targets) > 1 else 'target'} up!")
+
+        try:
+            for target in targets:
+                try:
+                    for i in range(port_range[0], port_range[1]):
+                        # logging.debug(f"{format(target)}:{i}")
+                        result = self.port_scan(format(target), i, scan_type)
+
+                        if result == "Open" or result == "Filtered":
+                            logging.info(f"{format(target)}:{i} - {result}")
+                except KeyboardInterrupt:
+                    return
+        except KeyboardInterrupt:
+            return
 
     def send_tcp(self, target: str, dport: int, flags: str):
         pkt = IP(dst=target) / TCP(dport=dport, flags=flags)
@@ -54,34 +182,3 @@ class PortScanner(Module):
                     return "Open"
                 elif res.getlayer(TCP).flags == 0x14:
                     return "Closed"
-
-    def main(self, target: str, port_range: [int, int], scan_type: str):
-        try:
-            for i in range(port_range[0], port_range[1]):
-                result = self.port_scan(target, i, scan_type)
-
-                if result == "Open" or result == "Filtered":
-                    logging.info(f"{target}:{i} - {result}")
-        except KeyboardInterrupt:
-            return
-
-    def initialize(self, options: dict):
-        self.arguments = options
-
-        if options[self.optname]:
-            begin_time = datetime.now()
-            logging.debug(f"Starting port scan!")
-
-            port = options["port"].split("-")
-            port = [int(i) for i in port]
-
-            scan_type = "TCP-SYN" if options["syn_scan"] else "TCP-Connect"
-
-            self.main(options["target"], port, scan_type)
-
-            logging.debug(f"Port scan finished in {datetime.now() - begin_time}")
-
-    def options(self, parser: argparse.ArgumentParser):
-        parser.add_argument("-sS", "--syn-scan", help="Execute SYN Scan (Stealth)", action="store_true")
-        parser.add_argument("-t", "--target", help="Target IP", type=str)
-        parser.add_argument("-p", "--port", help="Port range to scan", type=str)
